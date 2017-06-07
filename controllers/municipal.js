f742842ca8acb4389b3ae2dee9f6283b76312866
NachoBrovia <jibrovia@hotmail.com>
2017-06-07 12:03:13 -0300

    Cree controlador Municipal y le agregue las rutas correspondientes

---
+++ controllers/municipal.js
@@ -0,0 +1,163 @@
+'use strict'
+
+var fs = require('fs');
+var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
+var mongoosePaginate = require ('mongoose-Pagination');
+var Persona = require('../models/persona');
+var Usuario = require('../models/usuario');
+var jwt = require('../services/jwt');
+
+function getMunicipal(req, res){
+	var municipalId = req.params.id;
+    
+	Persona.findById(municipalId, (err, municipal) =>{
+		var usuarioId = municipal.usuario;
+
+		Usuario.findById(usuarioId, (err, usuario) =>{
+			if(err){
+			res.status(500).send({message: 'Error en la petición'});
+		}else{
+			if(usuario.rol != 'Municipal'){
+				res.status(404).send({message: 'El rol no es municipal'});
+			}else{
+				if(!municipal){
+				res.status(404).send({message: 'El municipal no existe'});
+				}else{
+				res.status(200).send({municipal});
+					}	
+				}
+			}
+		});	
+	});
+}
+
+function getMunicipales(req, res){
+	var usuarioId = req.params.Usuario;
+
+	if (!usuarioId) {
+		// sacar todos los conductores de la bd
+		var find = Persona.find({}).sort('apellido'); // sort es para ordenar
+	}else{
+		// sacar los conductores de un usuario concreto de la bd
+		var find = Persona.find({usuario: usuarioId}).sort('nombreUsuario');
+	}
+
+	find.populate({path: 'usuario'}).exec((err, municipales) =>{
+		if(err){
+			res.status(500).send({message: 'Error en la petición'});
+		}else{
+			if(!municipales){
+				res.status(404).send({message: 'No hay municipales'});
+			}else{
+				res.status(200).send({municipales});
+			}
+		}
+	});
+}
+
+function saveMunicipal(req, res){
+	// Este metodo guarda un usuario primero y luego el conductor con el objeto de usuario
+
+	var usuario = new Usuario();
+
+	var params = req.body;
+
+	usuario.nombreUsuario = params.nombreUsuario;
+	usuario.rol = "Municipal";
+	usuario.email = params.email;
+
+	if(params.clave){
+		// Encriptar contraseña y guardar datos
+		bcrypt.hash(params.clave,null,null,function(err,hash){
+			usuario.clave = hash;
+
+			if(usuario.nombreUsuario != null  && usuario.email != null){
+				// Guarda el usuario
+				usuario.save((err,usuarioStored) => {
+					if(err){
+						res.status(500).send({message: 'Error al guardar el usuario'});
+					}else{
+						if(!usuarioStored){
+							res.status(404).send({message: 'No se ha registrado el usuario'});
+						}else
+						  {
+							res.status(200).send({usuario: usuarioStored});
+						  }
+					}
+				});
+
+			}else{
+				res.status(200).send({message: 'Rellene todos los cambios'});
+			}
+		});
+
+	}else{
+		res.status(500).send({message: 'Introduce la contraseña'});
+	}
+
+	var persona = new Persona();
+
+	persona.nombre = params.nombre;
+	persona.apellido = params.apellido;
+	persona.dni = params.dni;
+	persona.telefono = params.telefono;
+	persona.usuario = usuario._id;
+
+
+	persona.save((err, municipalStored) => {
+		if(err){
+			res.status(500).send({message:'Error al guardar el municipal'});
+		}else{
+			if(!municipalStored){
+				res.status(404).send({message:'El municipal no ha sido guardado'});
+			}else{
+				res.status(200).send({persona: municipalStored});
+			}
+		}
+	});
+}
+
+function updateMunicipal(req, res){
+	var municipalId = req.params.id;
+	var update = req.body;
+
+	Persona.findByIdAndUpdate(municipalId, update, (err, municipalUpdated) =>{
+		if(err){
+			res.status(500).send({message:'Error al actualizar el municipal'});
+		}else{
+			if(!municipalUpdated)
+			{
+				res.status(404).send({message:'El municipal no ha sido actualizado'});
+			}else{
+				res.status(200).send({persona: municipalUpdated});
+			}
+		}
+	});
+}
+
+// Ver bien si vamos a usar o no este metodo
+
+function deleteMunicipal(req, res){
+	var municipalId = req.params.id;
+
+	Persona.findByIdAndRemove(municipalId, (err, municipalRemoved) =>{
+		if(err){
+			res.status(500).send({message: 'Error en el servidor'});
+		}else{
+			if(!municipalRemoved){
+				res.status(404).send({message: 'No se ha borrado el municipal'});
+			}else{
+				res.status(200).send({persona: municipalRemoved});
+			}
+		}
+	});
+}
+
+module.exports = {
+	saveMunicipal,
+	updateMunicipal,
+	deleteMunicipal,
+	getMunicipal,
+	getMunicipales
+};
--- routes/persona.js
+++ routes/persona.js
@@ -4,6 +4,8 @@ var express = require('express');
 
 var ConductorController = require('../controllers/conductor');
 var OperadorController = require('../controllers/operadorDePlaya');
+var MunicipalController = require('../controllers/municipal');
+
 
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
@@ -23,6 +25,14 @@ api.get('/traerOperadores/:page?', OperadorController.getOperadores);
 api.post('/registrarOperador', OperadorController.saveOperador);
 api.put('/modificarOperador/:id', OperadorController.updateOperador);
 api.delete('/eliminarOperador/:id', OperadorController.deleteOperador);
+
+// Municipales
+
+api.get('/traerMunicipal/:id',  MunicipalController.getMunicipal);
+api.get('/traerMunicipales/:page?', MunicipalController.getMunicipales);
+api.post('/registrarMunicipal', MunicipalController.saveMunicipal);
+api.put('/modificarMunicipal/:id', MunicipalController.updateMunicipal);
+api.delete('/eliminarMunicipal/:id', MunicipalController.deleteMunicipal);
 
 
 
